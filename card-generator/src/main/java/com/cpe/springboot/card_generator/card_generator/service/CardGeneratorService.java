d1e74d126c8ca80ec65d478b6d3319413274afef
Ewan <Ewan@DESKTOP-R449DOK>
2024-10-17 17:04:47 +0200

    add CardRequestDTO + generateCard

--- card-generator/src/main/java/com/cpe/springboot/card_generator/card_generator/service/CardGeneratorService.java
+++ card-generator/src/main/java/com/cpe/springboot/card_generator/card_generator/service/CardGeneratorService.java
@@ -1,7 +1,32 @@
 package com.cpe.springboot.card_generator.card_generator.service;
 
+import com.cpe.springboot.dto.CardDTO;
 import org.springframework.stereotype.Service;
+import com.cpe.springboot.dto.CardRequestDTO;
+import com.cpe.springboot.dto.CardRequestDTO;
+
 
 @Service
 public class CardGeneratorService {
+
+        public void generateCard(CardRequestDTO cardRequestDTO) {
+            CardDTO cardDTO = new CardDTO();
+
+            cardDTO.setHp(cardRequestDTO.getHp());
+            cardDTO.setEnergy(cardRequestDTO.getEnergy());
+            cardDTO.setAttack(cardRequestDTO.getAttack());
+            cardDTO.setDefence(cardRequestDTO.getDefense());
+
+            cardDTO.setPrice(999);//Où on récup le prix ?
+
+            cardDTO.setUserId(999);//A changer quand on le récup dans le body de la requete
+
+            cardDTO.setImgUrl(cardRequestDTO.getBase64Image());
+
+            cardDTO.setId(null);//ID pas encore géré ?
+
+            publishCardToQueue(CardDTO);//A implementer
+
+            System.out.println("Card generated :" + cardDTO);
+        }
 }
