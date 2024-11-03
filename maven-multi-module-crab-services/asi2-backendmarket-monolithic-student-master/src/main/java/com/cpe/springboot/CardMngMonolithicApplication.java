efe8406166640713408023a4629d045c067a2d11
pingoleon <steven.arnaud@outlook.fr>
2024-11-03 23:53:09 +0100

    ComponentsScan activemq bean in monolith

--- maven-multi-module-crab-services/asi2-backendmarket-monolithic-student-master/src/main/java/com/cpe/springboot/CardMngMonolithicApplication.java
+++ maven-multi-module-crab-services/asi2-backendmarket-monolithic-student-master/src/main/java/com/cpe/springboot/CardMngMonolithicApplication.java
@@ -2,6 +2,7 @@ package com.cpe.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jms.annotation.EnableJms;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 @EnableJms
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Card Market Rest Api", version = "1.0", description = "Information about the Card Market APi and how to interact with"))
+@ComponentScan({"com.cpe.springboot.activemq", "com.cpe.springboot.card", "com.cpe.springboot.store", "com.cpe.springboot.user"})
 // doc here localhost:8080/swagger-ui.html
 public class CardMngMonolithicApplication {
 
