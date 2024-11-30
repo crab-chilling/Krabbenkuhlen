e5627ef4149c40e26e5b7ca4266e1f89c754215f
yoann <yoanncathelain23@gmail.com>
2024-11-30 22:50:10 +0100

    split server node

---
+++ nodejs-backend/nodejs-game-backend/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import gameSocket from "./sockets/GameSocket.js";
+import cors from "cors";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  },
+});
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  }),
+);
+
+// Middlewares
+// app.use(express.json());
+app.use(express.static("www"));
+
+// Routes
+app.get("/health-check", (req, res) => {
+  res.send("UP");
+});
+
+// Websocket
+gameSocket(io);
+
+// Consumers
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
